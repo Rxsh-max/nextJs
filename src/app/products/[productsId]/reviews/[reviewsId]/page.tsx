fb19f1046fccb1ec0e1e10532275712b4b56948f
Rxsh-max <nikhil99kr99@gmail.com>
2025-07-06 01:01:07 +0530

    not-found fn in reviewsId

--- src/app/products/[productsId]/reviews/[reviewsId]/page.tsx
+++ src/app/products/[productsId]/reviews/[reviewsId]/page.tsx
@@ -1,6 +1,10 @@
+  import {notFound} from 'next/navigation'
   export default async function reviewsDetails({
     params,
-  }: {params : Promise<{productsId:String , reviewsId:String}>}){
+  }: {params : Promise<{productsId:String , reviewsId:string}>}){
     const {productsId,reviewsId} = await params;
+    if(parseInt(reviewsId)>100){
+        notFound();
+    }
     return <h1>review {reviewsId} for product {productsId}</h1>
   }
