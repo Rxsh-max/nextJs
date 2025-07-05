6a06e2b4c9e556df4685c2aac867388eedb02c5f
Rxsh-max <nikhil99kr99@gmail.com>
2025-07-06 01:21:15 +0530

    pathname hook

--- src/app/products/[productsId]/reviews/[reviewsId]/not-found.tsx
+++ src/app/products/[productsId]/reviews/[reviewsId]/not-found.tsx
@@ -1,6 +1,12 @@
+"use client"
+import { usePathname } from "next/navigation"
+
 export default function NotFound(){
+    const pathname = usePathname()
+    const productsId = pathname.split("/")[2]
+        const reviewsId = pathname.split("/")[4]
+
 return<>
-    <h1>review Not found</h1>
-    <h1>Could not found requested resource</h1>
+    <h1>review {reviewsId} Not found for product {productsId} </h1>
 </>
 } 
